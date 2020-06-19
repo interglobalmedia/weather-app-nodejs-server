675388a1f515a0aa3a54e0e5f30ac67afb165d38
interglobalmedia <interglobalmedia@gmail.com>
2020-06-19 11:45:55 -0400

    Add files first commit

---
+++ server.js
@@ -0,0 +1,70 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const env = require('./config/env')
+const app = express()
+const port = env.PORT || 3000
+
+// set up static directory to serve
+const publicDirectoryPath = path.join(__dirname, 'public')
+// set up view engine directory
+const viewsPath = path.join(__dirname, 'templates')
+const partialsPath = path.join(__dirname, 'templates/partials')
+
+app.set('views', viewsPath)
+app.set('view engine', 'hbs')
+hbs.registerPartials(partialsPath)
+
+// define path for Express config
+app.use(express.static(publicDirectoryPath))
+//home page route
+app.get('/', (req, res) => {
+    res.render('pages/index', {
+        title: 'Weather',
+        name: 'Maria D. Campbell',
+        message: 'Use this site to get your weather updates!',
+    })
+})
+// about route
+app.get('/about', (req, res) => [
+    res.render('pages/about', {
+        title: 'About me',
+        name: 'Maria D Campbell',
+    }),
+])
+// help route
+app.get('/help', (req, res) => {
+    res.render('pages/help', {
+        title: 'Help',
+        name: 'Maria D. Campbell',
+        message:
+            'For help with accessing the weather, please email Maria at interglobalmedia@gmail.com.',
+    })
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: `Cloudy`,
+        location: `New York`,
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('pages/404', {
+        title: '404',
+        name: 'Maria D. Campbell',
+        errorMessage: 'Help article not found. Try again.',
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('pages/404', {
+        title: '404',
+        errorMessage: 'Page not found.',
+        name: 'Maria D. Campbell',
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is up on Port 3000...`)
+})
