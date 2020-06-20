66f8fa1177578425c9cfc62f0d6e832809503d27
interglobalmedia <interglobalmedia@gmail.com>
2020-06-19 21:11:08 -0400

    Add fetch for client side weeather

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
-console.log('Client side JavaScript is loaded!')
+fetch(`http://localhost:3000/weather?address=!`).then((res) => {
+    res.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(`${data.location} ${data.forecast}`)
+        }
+    })
+})
